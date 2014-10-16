119c71ed1ff417aaf0bd8354b835d862b76d343e
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-10-16 10:10:43 +0100

    Refactor shopping_basket and tidy up comments

--- lib/shopping_basket.rb
+++ lib/shopping_basket.rb
@@ -7,26 +7,15 @@ class ShoppingBasket
 
 	def checkout
 		count = 0
-		sets = 0
 		discounted = []
-		#puts "books array before loop: " + @books.to_s
+
 		@books.uniq{ |u_book| u_book.id }.each do |book|
 			count += 1
-			#puts "count is: " + count.to_s
 			@books.delete_at(@books.index(book))
-			#puts "books array after: " + @books.to_s
 		end
+		
 		@sets_to_discount << count
-		#puts "sets to discount is: " + @sets_to_discount.to_s
-		#discount_set(count)
-
-		if !@books.empty?
-			checkout
-		else
-			#puts "total at end is: " + @total.to_s
-			discount_sets
-			@total
-		end
+		charge
 	end
 
 	def discount_sets
@@ -40,10 +29,17 @@ class ShoppingBasket
 				discount_set(set)
 			end
 		end
-
 	end
 
-	# unused method that was called above (comment out as well) before discount_sets existed
+	def charge
+		if !@books.empty?
+			checkout
+		else
+			discount_sets
+			@total
+		end
+	end
+
 	def discount_set(books_in_set)
 		@total += 8.00 if books_in_set == 1
 		@total += (16 * 0.95) if books_in_set == 2
